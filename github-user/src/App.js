f83f338bd634e244744b01dde3769b4219b4cff0
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-01 15:35:30 +0100

    got the axios working. Now working on display

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
+import CardDisplay from './CardDisplay';
 
-function App() {
-  return (
-    <div className="App">
-      Test App
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: [],
+    }
+  }
+
+  //functions
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/jaynecn')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+  }
+
+
+
+  render(){
+    return (
+      <div>
+        Test App II
+        <CardDisplay />
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ github-user/src/CardDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class CardDisplay extends React.Component {
+  render(props){
+    return (
+      <div>
+        Hello from the Card Display.
+      </div>
+    )
+  }
+
+}
